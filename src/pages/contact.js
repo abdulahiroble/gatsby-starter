b0a223a4a2ee55955fbb0b596885bd1e4f48eb88
abdulahiroble <abdulahi_2610@hotmail.com>
2019-12-03 13:20:20 +0100

    test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,7 @@ const Contact = ({ data }) => {
       <SEO postNode={postNode} pagePath="contact" customTitle />
 
       <Container>
-        <PageTitle>Kontakt</PageTitle>
+        <PageTitle>Kontakt os</PageTitle>
         <ContactForm />
       </Container>
     </Layout>
